ded2f406e0c07034b5be944804a593e80b4b4750
P S Shrijith <shrijithps89@gmail.com>
2023-03-15 01:23:36 +0530

    variables

--- JS Basics/variables.js
+++ JS Basics/variables.js
@@ -5,3 +5,8 @@ let firstName = "Jimmy"
 let lastName = "Carter"
 
 console.log('The name is '+firstName+" "+lastName)
+
+let radius = 5
+const PI = 3.14
+
+console.log("The area of the circle is "+3.14*(radius*radius))
