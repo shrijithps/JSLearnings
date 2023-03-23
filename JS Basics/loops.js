f46100a14d2a959b99b683d5bae788bf17b4defc
P S Shrijith <shrijithps89@gmail.com>
2023-03-23 22:25:26 +0530

    LoopsInJS

---
+++ JS Basics/loops.js
@@ -0,0 +1,19 @@
+let str = "test"
+let n = 4
+
+for(let i=0;i<str.length;i++){
+    console.log(str[i]);
+}
+
+for(i in "shrijith"){
+    console.log(i);
+}
+
+for(j of str){
+    console.log(j);
+}
+
+while(n>0){
+    console.log(n);
+    n--;
+}
