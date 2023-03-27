2f58049e11f37a020636d8ccb64e404322eafd55
P S Shrijith <shrijithps89@gmail.com>
2023-03-27 09:42:52 +0530

    finding Duplicates In a String

--- BasicPrograms/duplicatesInaString.js
+++ BasicPrograms/duplicatesInaString.js
@@ -2,5 +2,10 @@ let string = "Big black bug bit a big black dog on his big black nose".split(" "
 const set = new Set();
 
 for(let j of string){
-    console.log(j);
+    if(set.has(j)){
+        console.log("Element already exists - ", j)
+    }
+    else{
+        set.add(j);
+    }
 }
