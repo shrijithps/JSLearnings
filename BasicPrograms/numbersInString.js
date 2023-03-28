d872ff44b7b25b875e9c54c330f54c2157673623
P S Shrijith <shrijithps89@gmail.com>
2023-03-28 07:11:51 +0530

    numbers in a string

---
+++ BasicPrograms/numbersInString.js
@@ -0,0 +1,10 @@
+let s = "sentence23"
+
+for(let j of s){
+    if(j>='0' && j<='9'){
+        console.log("It is a number")
+    }
+    else{
+        console.log("It is a string character")
+    }
+}
